1413e8d70ce97e3e986ea45bf80ba617d8df917d
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-02-09 17:10:50 +0300

    Update SpringFrameworkApplication.java

--- springFramework/src/main/java/com/spring/springFramework/SpringFrameworkApplication.java
+++ springFramework/src/main/java/com/spring/springFramework/SpringFrameworkApplication.java
@@ -3,7 +3,6 @@ package com.spring.springFramework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SpringFrameworkApplication {
 
